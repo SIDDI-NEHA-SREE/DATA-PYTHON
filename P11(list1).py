a5767fe7da8cdb094910b9fedba44f7e9feeaf27
SIDDI-NEHA-SREE <siddinehasree@gmail.com>
2025-09-04 15:20:12 +0530

    Delete P11(list1).py

--- P11(list1).py
+++
@@ -1,9 +0,0 @@
-# list of x to y-1
-def nlist(n,m):
-    l=[]
-    for i in range(n,m,2):
-        l.append(i)
-    return l
-
-n,m=map(int,input("Enter n,m sep with comma : ").split(","))
-print(nlist(n,m))
